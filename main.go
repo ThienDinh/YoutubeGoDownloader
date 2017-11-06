32ad61894f5097831e7d5e6516f5cd36d8a9b55f
Thien Dinh <tdinhcs@gmail.com>
2017-11-05 21:54:59 -0500

    Add simple GET route to request download link.

--- main.go
+++ main.go
@@ -2,34 +2,26 @@ package main
 
 import (
 	"fmt"
-	"github.com/rylio/ytdl"
-	"os"
+	"net/http"
 )
 
 func main(){
-	fmt.Println("Hello Go, Long time no see!")
-	if len(os.Args) <= 1 {
-		fmt.Println("No Youtube ID provided!")
-		return
-	}
-	videoInfor, err :=ytdl.GetVideoInfoFromID(os.Args[1])
-	if(err != nil) {
-		fmt.Println("An error occurred!")
-	} else {
-		fmt.Println((*videoInfor).Title)
-		fmt.Println((*videoInfor).Description)
-		formatList := (*videoInfor).Formats.Filter(ytdl.FormatVideoEncodingKey,
-			[](interface{}){""}).Filter(ytdl.FormatAudioEncodingKey, [](interface{}){"aac", "mp3"})
-		for _, format := range formatList {
-			fileName := fmt.Sprintf("downloaded video/%s_%d.%s", "video", format.Itag, format.Extension)
-			os.Remove(fileName)
-			file, err := os.Create(fileName)
-			if err != nil {
-				fmt.Println("Error!")
-			} else {
-				videoInfor.Download(format, file)
-				fmt.Printf("%v\n", format)				
-			}
-		}
-	}
+	// fmt.Println("Hello Go, Long time no see!")
+	// if len(os.Args) <= 1 {
+	// 	fmt.Println("No Youtube ID provided!")
+	// 	return
+	// }
+
+	http.HandleFunc("/download", func(w http.ResponseWriter, r *http.Request) {
+		r.ParseForm()
+		fmt.Println("Request was:")
+		videoId := (*r).Form["id"][0]
+		link := Download(videoId)
+		fmt.Println(videoId)
+		fmt.Fprintf(w, fmt.Sprintf("<a target=\"_blank\" href=\"%s\">Click to download the audio file.</a>", link))
+	})
+
+	http.ListenAndServe(":80", nil)
+
+	
 }
