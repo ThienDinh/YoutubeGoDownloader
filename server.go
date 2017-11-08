a045aa8b679d56ed282930ea48d49bc24f7d0c60
Thien Dinh <tdinhcs@gmail.com>
2017-11-07 22:26:46 -0500

    Add duration into the response.

--- server.go
+++ server.go
@@ -4,21 +4,24 @@ import (
 	"fmt"
 	"github.com/rylio/ytdl"
 	"net/url"
+	"time"
 	// "os"
 )
 
 type YoutubeVideo struct {
 	Id string
 	Title string
+	Duration time.Duration
 	Extension string
-	Link string
+	VideoLink string
+	ThumbnailLink string
 }
 
 func (yv YoutubeVideo) String() string{
-	return fmt.Sprintf("%s\n%s\n%s\n%s", yv.Id, yv.Title, yv.Extension, yv.Link)
+	return fmt.Sprintf("%s\n%s\n%d\n%s\n%s\n%s", yv.Id, yv.Title, yv.Duration, yv.Extension, yv.VideoLink, yv.ThumbnailLink)
 }
 
-func Download(videoId string)  (youtubeVideo YoutubeVideo){	
+func Download(videoId string)  (youtubeVideo YoutubeVideo){
 	videoInfor, err :=ytdl.GetVideoInfoFromID(videoId)
 	if(err != nil) {
 		fmt.Println("An error occurred!")
@@ -35,7 +38,10 @@ func Download(videoId string)  (youtubeVideo YoutubeVideo){
 				// videoInfor.Download(format, file)
 				downloadUrl, _ := videoInfor.GetDownloadURL(format)
 				link, _ := url.QueryUnescape(downloadUrl.String())
-				youtubeVideo = YoutubeVideo{Id: videoId, Title:(*videoInfor).Title, Extension: format.Extension, Link: link}
+				// thumbnail := url.QueryUnescape("")
+				thumbnail := videoInfor.GetThumbnailURL(ytdl.ThumbnailQualityMaxRes).String()
+				youtubeVideo = YoutubeVideo{Id: videoId, Title:(*videoInfor).Title, Extension: format.Extension, Duration: (*videoInfor).Duration,
+					VideoLink: link, ThumbnailLink: thumbnail}
 				fmt.Printf("%v\n", youtubeVideo)				
 			// }
 		}
